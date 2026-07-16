b3da1198e381b30c66e63faa677b3730ce76fc19
sweetyboykzn-sudo <sweetyboykzn@gmail.com>
2026-07-16 11:22:03 +0300

    Вынес backend в серверную папку, добавил package.json для сервера

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
-import React from 'react';
+
 require('dotenv').config();
 
 const productsRoutes = require('./routes/products');
